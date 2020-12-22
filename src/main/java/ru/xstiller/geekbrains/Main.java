6e29911a8c11b82a9979782b7f752e747f09255a
FriezZ <slabunov.a@bk.ru>
2020-12-22 21:58:45 +0600

    added task number 8

--- src/main/java/ru/xstiller/geekbrains/Main.java
+++ src/main/java/ru/xstiller/geekbrains/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //задание №2
         byte b = 123;
         short s = 1234;
         int i = 321321;
@@ -14,33 +15,45 @@ public class Main {
 
     }
 
+    //задание №3
     public static float f1(float a, float b, float c, float d) {
         return a * (b + (c / d));
 
     }
 
+    //задание №4
     public static boolean b1(int a, int b) {
         int sum = a + b;
         return sum >= 10 && sum <= 20;
     }
 
+    //задание №5
+    public static void printIsNegative(int a) {
+        if (a < 0) {
+            System.out.println("Число " + a + " отрицательное");
+        } else {
+            System.out.println("Число " + a + " положительное");
+        }
+    }
+    //задание №6
     public static boolean isNegative(int a) {
         return a < 0;
 
     }
-
-    public static void printIsNegative(int a){
-        if(a < 0) {
-            System.out.println("Число " + a + " отрицательное");
-        }
-        else {System.out.println("Число " + a + " положительное");}
-    }
-
-    public static void printName(String name){
+    //задание №7
+    public static void printName(String name) {
         System.out.println("Привет, " + name);
     }
-
+    //задание №8*
+    public static void year(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год високосный");
+        } else {
+            System.out.println(year + " год не високосный");
+        }
+    }
 }
+
 
 
 
